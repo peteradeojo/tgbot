1514d270a0ac6d12d7149cf3bda48efd78002014
Peter Ade-Ojo <adeojopeter@gmail.com>
2025-09-06 03:10:10 +0100

    wip

--- main.py
+++ main.py
@@ -1,63 +1,142 @@
-import os
-import logging
-import dotenv
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, InlineQueryHandler
+import os, logging
+from sys import argv
+from dotenv import load_dotenv
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, User
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, ConversationHandler, MessageHandler
+from datetime import datetime
 
-from uuid import uuid4
+from database import Database
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+logger = logging.getLogger(__name__)
+
+NAME, TYPE, DATE, TIME = range(4)
+
+HELP_MESSAGE = '''
+Welcome to Party updates.
+
+Use the following commands to get around:
+/help - See this help message
+/register - Register your account to start using the bot
+/search - Search for Events happening around you
+/newevent - create an event 
+/events - see your upcoming events
+/edit - edit your event
+/delete - delete your event
+'''
+
+database: Database
+
+def check_user(user: User, context: ContextTypes.DEFAULT_TYPE):
+    dbuser = database.readone("SELECT id, username FROM users WHERE username = ?", [user.username])
+    
+    return dbuser
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, talk to me!")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
- 
-async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.inline_query.query
-    if not query:
+    user = update.effective_user
+    if user is None:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to Party updates!")
         return
-    results = []
-    results.append(
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    results.append(
-        InlineQueryResultArticle(
-            id=str(uuid4()),
-            title='Search',
-            input_message_content=InputTextMessageContent("What are you searching for werey?")
-        )
+    
+    database_user = database.readone(f"SELECT * FROM users WHERE username='{user.username}'")
+    if database_user is None:
+        database.insertone("INSERT INTO users (username, userId, createdAt) VALUES (?, ?, ?)", (user.username, user.id, datetime.now()))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to Party updates!")
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=HELP_MESSAGE)
+    
+async def newevent(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    dbuser = check_user(update.effective_user, context)
+    if dbuser is None:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You are not registered")
+        return
+    
+    # reply_keyboard = []
+    await update.message.reply_text(
+        "What is the name of the event you are trying to publicize?",
+        # reply_markup=ReplyKeyboardMarkup(
+        #     reply_keyboard, one_time_keyboard=True
+        # ),
     )
     
-    print(results)
+    return NAME
+
+async def eventname(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    name = update.message.text
+    logger.info("Event name: %s", name)
     
-    await context.bot.answer_inline_query(update.inline_query.id, results)
+    context.chat_data['event_name'] = name
+    
+    # await update.message.reply_text("Cool! Saved. Now what is the type of event? (e.g., party, meeting, etc.)")
+    reply_keyboard = [["Party", "Meeting", "Concert"], ["Workshop", "Other"]]
+    await update.message.reply_text(
+        "Cool! Saved. Now what is the type of event?",
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    )
+    
+    return TYPE
+    
+async def eventtype(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    type = update.message.text
+    logger.info("Event type: %s", type)
+    
+    if 'event_name' not in context.chat_data:
+        await update.message.reply_text("Please provide an event name first.")
+        return NAME
+    
+    event_name = context.chat_data['event_name']
+    
+    await update.message.reply_text(f"Event '{event_name}' of type '{type}' has been saved!") 
+    return ConversationHandler.END
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+def main():
+    token = os.environ.get('BOT_TOKEN')
+    if not token or token == "":
+        logging.error("Unable to retrieve bot token from environment. Add BOT_TOKEN to your environment.")
+        exit(1)
+    
+    app = ApplicationBuilder().token(token).build()
+    
+    # Handlers
+    start_handler=CommandHandler('start', start)
+    
+    app.add_handler(start_handler)
+    app.add_handler(CommandHandler('help', help))
+    app.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('newevent', newevent)],
+        states={
+            NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, eventname)],
+            TYPE: [MessageHandler(filters.Regex("^()$"), eventtype)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    ))
+        
+    app.run_polling()
 
 if __name__ == "__main__":
-    dotenv.load_dotenv(".env")
+    load_dotenv(".env")
+    database = Database()
     
-    app = ApplicationBuilder().token(str(os.environ.get('BOT_TOKEN'))).build()
-    
-    start_handler = CommandHandler('start', start)
-    caps_handler = CommandHandler('caps', caps)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    inline_caps_handler = InlineQueryHandler(inline_caps)
-   
-    app.add_handler(start_handler)
-    app.add_handler(caps_handler)
-    app.add_handler(echo_handler)
-    app.add_handler(inline_caps_handler)
-    
-    app.run_polling()
+    if len(argv) > 2:
+        if argv[1] == "migrations:show":
+            database.get_migrations()
+        elif argv[1] == "migrations:run":
+            database.run_migrations()
+        elif argv[1] == "migrations:revert":
+            database.revert_migrations(int(argv[2] if len(argv) == 3 else 1))
+    else:
+        main()
